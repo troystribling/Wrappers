b1d20a1d27d753ec022aab6dc19577f786e6cd60
Troy Stribling <troy.stribling@gmail.com>
2014-12-21 10:52:59 -0800

    created project

---
+++ Wrappers/Wrappers.h
@@ -0,0 +1,19 @@
+//
+//  Wrappers.h
+//  Wrappers
+//
+//  Created by Troy Stribling on 12/21/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Wrappers.
+FOUNDATION_EXPORT double WrappersVersionNumber;
+
+//! Project version string for Wrappers.
+FOUNDATION_EXPORT const unsigned char WrappersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wrappers/PublicHeader.h>
+
+
