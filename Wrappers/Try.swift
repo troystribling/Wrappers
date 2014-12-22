5a673114904667717b8a742944dda5f7d1dc0605
Troy Stribling <troy.stribling@gmail.com>
2014-12-22 09:01:44 -0800

    almost finished with Try

--- Wrappers/Try.swift
+++ Wrappers/Try.swift
@@ -87,15 +87,52 @@ public enum Try<T> {
         }
     }
     
-    public mutating func filter(predicate:T -> Bool) -> Try<T> {
+    public func filter(predicate:T -> Bool) -> Try<T> {
         switch self {
         case .Success(let box):
             if !predicate(box.value) {
-                self = .Failure(NSError(domain:TryError.domain, code:TryError.FilterFailed.code, userInfo:[NSLocalizedDescriptionKey:TryError.FilterFailed.description]))
+                return .Failure(NSError(domain:TryError.domain, code:TryError.FilterFailed.code, userInfo:[NSLocalizedDescriptionKey:TryError.FilterFailed.description]))
             }
             return self
         case .Failure(let error):
             return self
+        }
+    }
+    
+    public func foreach<M>(mapping:T -> M) {
+        switch self {
+        case .Success(let box):
+            box.map(mapping)
+            return
+        case .Failure:
+            return
+        }
+    }
+    
+    public func toOptional() -> Optional<T> {
+        switch self {
+        case .Success(let box):
+            return Optional<T>(box.value)
+        case .Failure(let error):
+            return Optional<T>()
+        }
+    }
+    
+    public func getOrElse(failed:T) -> T {
+        switch self {
+        case .Success(let box):
+            return box.value
+        case .Failure(let error):
+            return failed
+        }
+    }
+    
+    public func orElse(failed:Try<T>) -> Try<T> {
+        switch self {
+        case .Success(let box):
+            return self
+        case .Failure(let error):
+            return failed
         }
     }
     
@@ -117,10 +154,7 @@ public func recoverWith<T>(try:Try<T>, recovery:NSError -> Try<T>) -> Try<T> {
     return try.recoverWith(recovery)
 }
 
-public func isSuccess<T>(try:Try<T>) -> Bool {
-    return try.isSuccess()
+public func filter<T>(try:Try<T>, predicate:T -> Bool) -> Try<T> {
+    return try.filter(predicate)
 }
 
-public func isFailure<T>(try:Try<T>) -> Bool {
-    return try.isFailure()
-}
